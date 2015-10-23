3218fbcd3356c638514a4ee5a68ad8b1e7477147
Georgia <georgia.stuart@gmail.com>
2015-10-22 23:08:59 -0500

    Some comments

--- src/main.c
+++ src/main.c
@@ -16,11 +16,14 @@ int main(int argc, const char* argv[])
     dim.ydim = DIM;
     perm_strength = 1;
 
+    /* Reads in the permeability and source fields */
     perm = read_file(dim, "perm_field_small.txt");
     source = read_file(dim, "src_field.txt");
 
+    /* Initializes the mesh */
     mesh = setup_mesh(dim, perm, perm_strength, source);
 
+    /* Frees memory used from the read-in permeability and source fields */
     free(perm);
     free(source);
 
