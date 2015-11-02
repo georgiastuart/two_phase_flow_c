341a2ad56e332bd415c380d7aa2f938eb06a03fa
Georgia <georgia.stuart@gmail.com>
2015-11-01 23:04:15 -0600

    deleted intermediate pressure translating files

--- read_pressure.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-if __name__ == '__main__':
-    pressure = np.fromfile('output/pressure.dat', dtype = np.float64)
-
-    pressure_reshape = pressure.reshape((64, 64))
-    print pressure_reshape
-
-    np.savetxt('pressure_plaintxt.txt', pressure_reshape, delimiter = '\t', newline = '\n')
